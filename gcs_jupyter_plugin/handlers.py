ce5bda69bf58ef82d7281482e8510b6045868125
Harshavardhan <harshavardhan.s.r@accenture.com>
2025-06-25 19:15:31 +0530

    Adding the file logging capability and changed login to async

--- gcs_jupyter_plugin/handlers.py
+++ gcs_jupyter_plugin/handlers.py
@@ -3,7 +3,12 @@ import subprocess
 
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
+from traitlets import Unicode
+from traitlets.config import SingletonConfigurable
+
 import tornado
+
+from google.cloud.jupyter_config.config import async_run_gcloud_subcommand
 
 from gcs_jupyter_plugin import credentials
 from gcs_jupyter_plugin.controllers.gcs import (
@@ -17,19 +22,22 @@ from gcs_jupyter_plugin.controllers.gcs import (
     DownloadFileController,
 )
 
-
+class GcsPluginConfig(SingletonConfigurable):
+    log_path = Unicode(
+        "",
+        config=True,
+        help="File to log ServerApp and Gcs Jupyter Plugin events.",
+    )
+    
 class LoginHandler(APIHandler):
     @tornado.web.authenticated
     async def post(self):
-        cmd = "gcloud auth login"
-        process = subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
-        )
-        output, _ = process.communicate()
-        # Check if the authentication was successful
-        if process.returncode == 0:
+        try:
+            sub_cmd = "auth login"
+            await async_run_gcloud_subcommand(sub_cmd)
             self.finish({"login": "SUCCEEDED"})
-        else:
+        except subprocess.CalledProcessError as e:
+            self.log.exception("login failed with error: ", e)
             self.finish({"login": "FAILED"})
 
 
@@ -41,7 +49,7 @@ class CredentialsHandler(APIHandler):
     async def get(self):
         cached = await credentials.get_cached()
         if cached["config_error"] == 1:
-            self.log.exception(f"Error fetching credentials from gcloud")
+            self.log.exception("Error fetching credentials from gcloud")
         self.finish(json.dumps(cached))
 
 
@@ -63,7 +71,7 @@ def setup_handlers(web_app):
     def full_path(name):
         return url_path_join(base_url, application_url, name)
 
-    handlersMap = {
+    handlers_map = {
         "credentials": CredentialsHandler,
         "log": LogHandler,
         "login": LoginHandler,
@@ -76,5 +84,5 @@ def setup_handlers(web_app):
         "api/storage/renameFile": RenameFileController,
         "api/storage/downloadFile": DownloadFileController,
     }
-    handlers = [(full_path(name), handler) for name, handler in handlersMap.items()]
+    handlers = [(full_path(name), handler) for name, handler in handlers_map.items()]
     web_app.add_handlers(host_pattern, handlers)
